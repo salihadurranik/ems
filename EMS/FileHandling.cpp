213b5e614a0c560eaebbf82cb46b898a3b3e07e8
Saliha Durrani <96047694+salihadurranik@users.noreply.github.com>
2021-12-23 08:22:01 +0500

    Update FileHandling.cpp

--- EMS/FileHandling.cpp
+++ EMS/FileHandling.cpp
@@ -1,1 +1,21 @@
+#include <fstream>
+#include <iostream>
 #include "FileHandling.h"
+
+using namespace std;
+
+FileHandling::FileHandling(string fileName) {
+  FileName = fileName;  
+}
+
+template<class TEntity>
+void SetValue(TEntity entity) {
+  // Open file for writing data
+  FileWrite.open(FileName, ios::app);
+  
+  // Write object to the file
+  FileWrite.write((char*)&entity, sizeof(entity));
+  
+  // Close file
+  FileWrite.close();
+}
