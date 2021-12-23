9f778252570dabab870524e07b952be706e037d0
Saliha Durrani <96047694+salihadurranik@users.noreply.github.com>
2021-12-23 07:46:40 +0500

    Update FileHandling.h

--- EMS/FileHandling.h
+++ EMS/FileHandling.h
@@ -1,5 +1,14 @@
 #pragma once
+
+template <class TEntity>
 class FileHandling
 {
+  private:
+    TEntity PObject;
+  
+  public:
+    FileHandling();
+    void SetValue(TEntity entity);
+    TEntity GetValue();
 };
 
